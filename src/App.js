225b1db0050809dcc3f33f1e71b81066b8bec9a2
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-03-02 21:24:53 +0900

    add : 상단 요소 : 프로필 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import {Profile} from './Profile.js';
 
 function App() {
+
   return (
     <div className="App">
+
+      {/* 상단 항목 */}
+      <div>
+        <Profile></Profile>
+      </div>
+
+      {/* 하단 헤더 */}
+      <div>
+
+      </div>
+
+      {/* 하단 항목 */}
+
     </div>
   );
 }

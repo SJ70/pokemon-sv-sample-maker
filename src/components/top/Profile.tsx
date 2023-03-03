34eb6785d423c05d8307a6f0ce71d1722502eeec
SJ70 <50670730+SJ70@users.noreply.github.com>
2023-03-03 21:53:56 +0900

    img 파일명 및 경로 수정

--- src/components/top/Profile.tsx
+++ src/components/top/Profile.tsx
@@ -4,13 +4,13 @@ const Profile = () => {
     return(
         <div className='profile'>
             <input className='sampleName' placeholder='샘플명 입력'></input>
-            <img className='pokemon' src={ require('../../database/pokemon/img/000.png')} alt=""></img>
+            <img className='pokemon' src={ require('../../database/img/pokemon/000.png')} alt=""></img>
             <div className='nameAndTypes'>
                 <p className='name'>포켓몬 이름</p>
                 <div className='types'>
-                    <img className='type' src={ require('../../database/type/img/normal.png')} alt=""></img>
-                    <img className='type' src={ require('../../database/type/img/fire.png')} alt=""></img>
-                    <img className='teraType' src={ require('../../database/terastalType/img/fire.png')} alt=""></img>
+                    <img className='type' src={ require('../../database/img/type/0.png')} alt=""></img>
+                    <img className='type' src={ require('../../database/img/type/1.png')} alt=""></img>
+                    <img className='teraType' src={ require('../../database/img/terastalType/0.png')} alt=""></img>
                 </div>
             </div>
         </div>
